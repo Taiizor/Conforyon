e7bcb77d50f7f6cd7c63038b3419aac64fb9fda2
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-06 15:16:17 +0300

    53X Again 53!

--- src/Conforyon/Conforyon.cs
+++ src/Conforyon/Conforyon.cs
@@ -10,7 +10,7 @@ using System.Text.RegularExpressions;
 //     Creator: Taiizor
 //     Site   : www.Taiizor.com
 //     Created: 04.Jul.2019
-//     Changed: 05.Aug.2020
+//     Changed: 06.Aug.2020
 //     Version: 1.4.6.5
 //
 // |---------DO-NOT-REMOVE---------|
