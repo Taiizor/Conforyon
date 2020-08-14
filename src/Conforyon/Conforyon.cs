e27f76ed76e851284ef8b063c67ad32db45310ed
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-14 19:34:06 +0300

    53X Again 111!

--- src/Conforyon/Conforyon.cs
+++ src/Conforyon/Conforyon.cs
@@ -11,7 +11,7 @@ using System.Text.RegularExpressions;
 //     Site   : www.Taiizor.com
 //     Created: 04.Jul.2019
 //     Changed: 14.Aug.2020
-//     Version: 1.4.7.2
+//     Version: 1.4.7.3
 //
 // |---------DO-NOT-REMOVE---------|
 
