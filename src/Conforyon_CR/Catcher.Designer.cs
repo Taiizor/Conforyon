5de27220895d849697b5d22721408274179b7bc2
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-23 01:01:21 +0300

    53X Again 132!

--- src/Conforyon_CR/Catcher.Designer.cs
+++ src/Conforyon_CR/Catcher.Designer.cs
@@ -342,6 +342,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Catcher";
