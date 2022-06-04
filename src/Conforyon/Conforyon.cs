a154cfd13ff7333b4a650558b4c5a06248722806
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-06-04 18:15:15 +0300

    Minor Changes

--- src/Conforyon/Conforyon.cs
+++ src/Conforyon/Conforyon.cs
@@ -22,7 +22,7 @@ using STRER = System.Text.RegularExpressions.Regex;
 //     Website: www.Vegalya.com
 //     Created: 04.Jul.2019
 //     Changed: 04.Jun.2022
-//     Version: 2.0.0.2
+//     Version: 2.0.0.3
 //
 // |---------DO-NOT-REMOVE---------|
 
