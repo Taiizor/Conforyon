b6c71ee76b8c21eedf883d31ce00d92bd5c2a703
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-12-08 22:05:26 +0300

    Packages Updated

--- src/Conforyon_CR/Program.cs
+++ src/Conforyon_CR/Program.cs
@@ -12,7 +12,7 @@ namespace Conforyon_CR
         private static void Main()
         {
             Application.EnableVisualStyles();
-#if NET5_0 || NET6_0 || NET7_0
+#if NET6_0 || NET7_0
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
 #endif
             Application.SetCompatibleTextRenderingDefault(false);
